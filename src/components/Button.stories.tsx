976708e006e260716846241f7aa62fc397d83651
Hasibur Rahman <hasiburdev@gmail.com>
2023-02-03 01:53:08 +0600

    Add storybooks

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,27 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+
+import { Button } from "./Button";
+
+type ButtonProps = {
+  /** Button children element */
+  children: React.ReactNode;
+  /** Show loading spinner and disable button */
+  isLoading?: boolean;
+  /** Button color variant */
+  variants?: "primary" | "secondary";
+  /** Disable the button and add not-allowed cursor */
+  disabled?: boolean;
+} & React.ComponentPropsWithoutRef<"button">;
+
+const ButtonStory = {
+  title: "Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+export default ButtonStory;
+export const PrimaryButton = () => (
+  <Button className="bg-blue-600">Hello</Button>
+);
+export const SecondaryButton = () => (
+  <Button className="bg-red-600">Hello World!</Button>
+);
---
+++ src/components/Button.tsx
@@ -0,0 +1,3 @@
+export const Button = () => {
+  return <button className="rounded px-6 py-3">Hello World!</button>;
+};
